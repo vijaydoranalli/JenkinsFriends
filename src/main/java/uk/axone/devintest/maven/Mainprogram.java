afe4314e6028d6629d7cd1ee1743d9d1025d1229
vijaydoranalli <vijay.d999@yahoo.co.uk>
2020-06-23 16:46:58 +0100

    All tests

---
+++ src/main/java/uk/axone/devintest/maven/Mainprogram.java
@@ -0,0 +1,9 @@
+package uk.axone.devintest.maven;
+public class Mainprogram {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+
+
---
+++ src/test/java/uk/axone/devintest/maven/test/AppTests.java
@@ -0,0 +1,21 @@
+package uk.axone.devintest.maven.test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTests {
+
+@Test
+    public void test1()
+{
+    System.out.println("Running test1");
+}
+
+    @Test
+    public void test2()
+    {
+        System.out.println("Running test2");
+    }
+
+}
+
+
